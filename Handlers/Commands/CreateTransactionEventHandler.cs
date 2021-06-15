a085525a9534404c46bb512a0b6b72372ae96ca1
Jacob Tan <jacobkktan@gmail.com>
2021-06-15 21:40:17 +0800

    Add user id during transaction creation

--- Handlers/Commands/CreateTransactionEventHandler.cs
+++ Handlers/Commands/CreateTransactionEventHandler.cs
@@ -1,9 +1,11 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Webbpay.Api.PaymentService.Extensions;
 using Webbpay.Api.PaymentService.Mappers;
 using Webbpay.Api.PaymentService.Models.Commands;
 using Webbpay.Api.PaymentService.Models.Dtos;
@@ -16,16 +18,19 @@ namespace Webbpay.Api.PaymentService.Handlers.Commands
     {
         private readonly ITransactionEventRepository _transactionEventRepository;
         private readonly IMediator _mediator;
+        private readonly IHttpContextAccessor _httpContext;
 
-        public CreateTransactionEventHandler(ITransactionEventRepository transactionEventRepository, IMediator mediator)
+        public CreateTransactionEventHandler(ITransactionEventRepository transactionEventRepository, IMediator mediator, IHttpContextAccessor httpContext)
         {
             _transactionEventRepository = transactionEventRepository;
             _mediator = mediator;
+            _httpContext = httpContext;
         }
 
         public async Task<TransactionEventDto> Handle(CreateTransactionEventCommand request, CancellationToken cancellationToken)
         {
             var @event = request.Event.ToEntity();
+            @event.CreatedBy = _httpContext.HttpContext.User.GetUserId();
             @event = await _transactionEventRepository.Create(@event);
 
             var result = @event.ToModel();
