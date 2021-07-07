cedc0965f34eca200915658ffb94b2c270178a1f
Jacob Tan <jacobkktan@gmail.com>
2021-07-07 20:04:53 +0800

    Update razer settings

--- Models/PaymentLinkCacheModel.cs
+++ Models/PaymentLinkCacheModel.cs
@@ -95,5 +95,134 @@ namespace Webbpay.Api.PaymentService.Models
         public bool AllowGrabPay { get; set; }
         public bool AllowM2u { get; set; }
         public bool AllowShopeePay { get; set; }
+        public bool AllowRazerPay { get; set; }
+
+        public bool AllowFpxAmb { get; set; }
+
+        public bool AllowFpxBimb { get; set; }
+
+        public bool AllowFpxCimbClicks { get; set; }
+
+        public bool AllowFpxHlb { get; set; }
+
+        public bool AllowFpxMb2u { get; set; }
+
+        public bool AllowFpxPbb { get; set; }
+
+        public bool AllowFpxRhb { get; set; }
+
+        public bool AllowFpxOcbc { get; set; }
+
+        /// <summary>
+        /// Standard Chartered
+        /// </summary>
+        public bool AllowFpxScb { get; set; }
+
+        /// <summary>
+        /// Affin
+        /// </summary>
+        public bool AllowFpxAbb { get; set; }
+
+        /// <summary>
+        /// Alliance Bank
+        /// </summary>
+        public bool AllowFpxAbmb { get; set; }
+
+        public bool AllowFpxUob { get; set; }
+
+        public bool AllowFpxBsn { get; set; }
+
+        /// <summary>
+        /// Kuwait Finance House
+        /// </summary>
+        public bool AllowFpxKfh { get; set; }
+
+        /// <summary>
+        /// Bank Kerjasama Rakyat
+        /// </summary>
+        public bool AllowFpxBkrm { get; set; }
+
+
+        /// <summary>
+        /// Bank Muamalat
+        /// </summary>
+        public bool AllowFpxBmmb { get; set; }
+
+        public bool AllowFpxHsbc { get; set; }
+
+        /// <summary>
+        /// B2B Affin Bank
+        /// </summary>
+        public bool AllowFpxB2bAbb { get; set; }
+
+        /// <summary>
+        /// FPX B2B Ambank Berhad	
+        /// </summary>
+        public bool AllowFpxB2bAmb { get; set; }
+
+        /// <summary>
+        /// FPX B2B Bank Islam Malaysia Berhad (BIMB)	
+        /// </summary>
+        public bool AllowFpxB2bBimb { get; set; }
+
+        /// <summary>
+        /// FPX B2B BizChannel@CIMB (CIMB)	
+        /// </summary>
+        public bool AllowFpxB2bCimb { get; set; }
+
+        /// <summary>
+        /// FPX B2B HSBC	
+        /// </summary>
+        public bool AllowFpxB2bHsbc { get; set; }
+
+        /// <summary>
+        /// FPX B2B Public Bank	
+        /// </summary>
+        public bool AllowFpxB2bPbb { get; set; }
+
+        /// <summary>
+        /// FPX B2B RHB Reflex	
+        /// </summary>
+        public bool AllowFpxB2bRhb { get; set; }
+
+        /// <summary>
+        /// FPX B2B United Overseas Bank	
+        /// </summary>
+        public bool AllowFpxB2bUob { get; set; }
+
+        /// <summary>
+        /// FPX B2B Kuwait Finance House Overseas Bank	
+        /// </summary>
+        public bool AllowFpxB2bKfh { get; set; }
+
+        /// <summary>
+        /// FPX B2B Deutsche Bank	
+        /// </summary>
+        public bool AllowFpxB2bDeutsche { get; set; }
+
+        /// <summary>
+        /// FPX B2B Alliance Bank	
+        /// </summary>
+        public bool AllowFpxB2bAbmb { get; set; }
+
+        /// <summary>
+        /// FPX B2B Standard Chartered Bank	
+        /// </summary>
+        public bool AllowFpxB2bScb { get; set; }
+
+        /// <summary>
+        /// FPX B2B OCBC Bank	
+        /// </summary>
+        public bool AllowFpxB2bOcbc { get; set; }
+
+        /// <summary>
+        /// FPX B2B Bank Muamalat	
+        /// </summary>
+        public bool AllowFpxB2bBmmb { get; set; }
+
+        /// <summary>
+        /// FPX Maybank2e
+        /// </summary>
+        public bool AllowFpxB2bM2e { get; set; }
     }
 }
