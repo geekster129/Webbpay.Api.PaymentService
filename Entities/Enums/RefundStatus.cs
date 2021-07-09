a4b5b7eba8aead7776e2be0bab37ec4953ca9ebe
Jacob Tan <jacobkktan@gmail.com>
2021-07-09 15:58:18 +0800

    Refund status serialize as string

--- Entities/Enums/RefundStatus.cs
+++ Entities/Enums/RefundStatus.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Webbpay.Api.PaymentService.Entities.Enums
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum RefundStatus
     {
         Pending,
