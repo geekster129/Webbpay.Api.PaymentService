114c32947db1b5a59536a3d8fcc1956c726374cc
Mun Yau Lai <munyau@gmail.com>
2021-07-09 20:09:31 +0800

    Add successful transactions field

--- Migrations/20210709120722_Add successful payments field.Designer.cs
+++ Migrations/20210709120722_Add successful payments field.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Webbpay.Api.PaymentService.Adapters.Database;
 
 namespace Webbpay.Api.PaymentService.Migrations
 {
     [DbContext(typeof(PaymentDbContext))]
-    partial class PaymentDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210709120722_Add successful payments field")]
+    partial class Addsuccessfulpaymentsfield
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
